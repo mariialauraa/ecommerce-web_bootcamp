1d592e53de6e396219d7bf7d8059f6df2245b37e
mariialauraa <mlauraifg@gmail.com>
2023-10-25 10:39:00 -0300

    1.23 Preparando a paginação

--- components/shared/AdminListTable/index.tsx
+++ components/shared/AdminListTable/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import styles from '../../../styles/AdminPanel.module.css';
 import { Table } from 'react-bootstrap';
-import StyledButton from '../StyledButton';
+import Pagination from '../Pagination';
+import Meta from '../../../dtos/Meta';
 
 interface AdminListTableProps {
   children: React.ReactNode;
-  first_title: String,
-  second_title?: String,
-  third_title?: String,
-  fourth_title?: String,
-  fifth_title?: String,
-  sixth_title?: String
+  first_title: string;
+  second_title?: string;
+  third_title?: string;
+  fourth_title?: string;
+  fifth_title?: string;
+  sixth_title?: string;
+  meta?: Meta;
 }
 
-const AdminListTable: React.FC<AdminListTableProps> = ({children, first_title, second_title, third_title, fourth_title, fifth_title, sixth_title}) => {
+const AdminListTable: React.FC<AdminListTableProps> = ({children, first_title, second_title, third_title, fourth_title, fifth_title, sixth_title, meta}) => {
   return (
     <div className={styles.admin_panel}>
       <Table borderless={true} hover={true} responsive={true}>
@@ -35,17 +37,7 @@ const AdminListTable: React.FC<AdminListTableProps> = ({children, first_title, s
 
       </Table>  
 
-      <div className="pagination justify-content-end">
-        <div className="pagination">
-          <StyledButton action="<" type_button="blue" />
-          <StyledButton action="1" type_button="blue" />
-          <StyledButton action="2" type_button="blue" />
-          <StyledButton action="3" type_button="blue" />
-          ...
-          <StyledButton action="31" type_button="blue" />
-          <StyledButton action=">" type_button="blue" />
-        </div>
-      </div>    
+      <Pagination {...meta} />           
     </div>
   )
 }
