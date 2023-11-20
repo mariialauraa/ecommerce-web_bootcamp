02349034d23695aabc21138df10df5d8c6fb77f6
mariialauraa <mlauraifg@gmail.com>
2023-11-20 14:56:19 -0300

    Storefront 1.5 Ajustando o storefront component (home)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,44 @@
 import MainComponent from '../components/shared/MainComponent';
+import { Carousel } from 'react-bootstrap';
+import styles from './styles.module.css';
+import HightlightedProducts from '../components/Storefront/HighlightedProducts';
 
-import HightlightedProduct from '../components/Storefront/HighlightedProducts'
-
-const Home: React.FC = () => {
+const Storefront: React.FC = () => {
   return (
     <MainComponent>
-      <h1>Home</h1>
-      <HightlightedProduct title="Promoção"/>
-      <HightlightedProduct title="Em destaque" type="highlighted"/>
+      <Carousel className={styles.carousel}>
+        <Carousel.Item>
+        <img 
+            className="d-block w-100"
+            src="https://meups.com.br/wp-content/uploads/2018/01/God-of-War-4-900x503.jpg" 
+            alt="First Slide"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img 
+            className="d-block w-100"
+            src="https://meups.com.br/wp-content/uploads/2018/01/God-of-War-4-900x503.jpg" 
+            alt="Second Slide"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img 
+            className="d-block w-100"
+            src="https://meups.com.br/wp-content/uploads/2018/01/God-of-War-4-900x503.jpg" 
+            alt="Third Slide"
+          />
+        </Carousel.Item>
+      </Carousel>
+
+      <HightlightedProducts title="Ofertas da Semana" type="highlighted" />
+
+      <HightlightedProducts title="Lançamentos" />
+
+      <HightlightedProducts title="Mais Populares" />
     </MainComponent>
   )
 }
 
-export default Home;
+export default Storefront;
