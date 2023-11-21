f824900beacbf07d54b5f9798e1ca84d7846edb3
mariialauraa <mlauraifg@gmail.com>
2023-11-21 16:29:30 -0300

    Storefront 1.13 Ajustando nossa pesquisa no header

--- components/shared/Header/StorefrontHeader/index.tsx
+++ components/shared/Header/StorefrontHeader/index.tsx
@@ -1,54 +1,85 @@
-import React from 'react';
+import { useState } from 'react';
 import styles from './styles.module.css';
 import { InputGroup, FormControl, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faShoppingCart, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import Logo from '../../Logo';
-
-{/* importação do Link para redirecionarmos o usuário para o login */}
 import Link from 'next/link';
 
-const StorefrontHeader: React.FC = () => {
-  return (
-    <Row className={styles.background}>
-      <Col md={6} className="mt-2">
-        <Logo  />
-      </Col>
+import ProductSearchService from '../../../../util/ProductSearchService';
+import { useRouter } from 'next/router';
 
-      {/* ajustando o tamanho do input de pesquisa e corrigindo o placeholder */}
-      <Col md={6} className="mt-2 text-center">
-        <Row>
-          <Col md={6} className="mb-4 mb-md-0">
-            <InputGroup>
-              <FormControl placeholder="Pesquisar produto" />
-            </InputGroup>
-          </Col>
+const CustomerHeader: React.FC = () => {
 
-          <Col md={6}>
-            <Row>
-              <Col md={4} xs={4}>
-                <FontAwesomeIcon icon={faSearch} color="var(--color-gray-light)" />
-              </Col>
+    //estado de pesquisa, começa 'vazio'
+    const [search, setSearch] = useState('');
 
-              <Col md={4} xs={4}>
-                <FontAwesomeIcon icon={faShoppingCart} color="var(--color-gray-light)" />
-              </Col>
+    //redireciona o usuário para página de pesquisa
+    const router = useRouter();
 
-              {/* adicionando o link para que quando o usuário clicar no ícone de usuário o mesmo
-              seja redirecionado para o login */}
-              <Col md={4} xs={4}>
-                <Link href="/Auth/Login">
-                  <a>
-                    <FontAwesomeIcon icon={faUserCircle} color="var(--color-gray-light)" />
-                  </a>
-                </Link>
-              </Col>
-            </Row>
-          </Col>
+    //qdo executar a pesquisa vai redirecionar p/ a rota de pesquisa
+    const handleSearch = (): void => {
+        router.push(`/Search${ProductSearchService.execute({ search })}`);
+    }
+
+    return (
+        <Row className={styles.background}>
+            <Col md={6} className="mt-2">
+                <Logo />
+            </Col>
+
+            <Col md={6} className="mt-2 text-center">
+                <Row>
+                    <Col md={6} className="mb-4 mb-md-0">
+                        <InputGroup
+                            className={`${router.pathname === '/Search' ? styles.hidden : ''}`}
+                        >
+                            <FormControl 
+                                placeholder="Pesquisar produto" 
+                                value={search}
+                                onChange={
+                                    (evt: React.ChangeEvent<HTMLInputElement>) =>
+                                        setSearch(evt.target.value)
+                                }
+
+                                onKeyUp={
+                                    (evt: React.KeyboardEvent<HTMLInputElement>) => {
+                                        if (evt.key.toLowerCase() === 'enter') {
+                                            handleSearch();
+                                        }
+                                    }
+                                }
+                            />
+                        </InputGroup>
+                    </Col>
+
+                    <Col md={6}>
+                        <Row>
+                            <Col className={`${router.pathname === '/Search' ? styles.hidden : ''}`}>
+                                <FontAwesomeIcon 
+                                    icon={faSearch} 
+                                    color="var(--color-gray-light)" 
+                                    onClick={handleSearch}
+                                />
+                            </Col>
+
+                            <Col>
+                                <FontAwesomeIcon icon={faShoppingCart} color="var(--color-gray-light)" />
+                            </Col>
+
+                            <Col>
+                                <Link href="/Auth/Login">
+                                    <a>
+                                        <FontAwesomeIcon icon={faUserCircle} color="var(--color-gray-light)" />
+                                    </a>
+                                </Link>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Col>
         </Row>
-      </Col>
-    </Row>
-  )
+    )
 }
 
-export default StorefrontHeader;
+export default CustomerHeader;
