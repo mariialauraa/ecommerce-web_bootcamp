b78a586cafc38b0a65dc4b36e6e257656b440354
mariialauraa <mlauraifg@gmail.com>
2023-11-27 09:32:40 -0300

    Storefront 1.32 Obtendo os itens da Wishlist

--- services/wishlist.ts
+++ services/wishlist.ts
@@ -1,6 +1,16 @@
 import api from './api';
+import ProductSearch from '../dtos/ProductSearch';
 
+interface WishlistIndexData {
+  wish_items: ProductSearch[];
+}
+
+//método 'index' para obter os produtos da lista de desejos do usuário logado
 const WishlistService = {
+  index(url: string) {
+    return api.get<WishlistIndexData>(url).then(response => response.data);
+  },
+
   add(productId: number) {
     return api.post<void>('/storefront/v1/wish_items', { wish_item: { product_id: productId } });
   }
