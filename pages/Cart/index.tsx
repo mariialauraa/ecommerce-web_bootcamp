f4fdbb068383fd2d495dc3c04f10b02c37d85a42
mariialauraa <mlauraifg@gmail.com>
2023-12-01 12:29:33 -0300

    Storefront 2.13 Exibindo itens na página de carrinho

--- pages/Cart/index.tsx
+++ pages/Cart/index.tsx
@@ -11,11 +11,22 @@ import withAuth from '../../components/withAuth';
 
 import CheckoutForm from '../../components/Storefront/CheckoutForm';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCartProduct } from '../../store/modules/storefront/cartProducts/reducer';
+import ProductShow from '../../dtos/ProductShow';
+
 const Cart: React.FC = () => {
+  const dispatch = useDispatch();
+  const cartProducts: ProductShow[] = useSelector(state => state.cartProducts);
+
+  const handleRemove = (index: number): void => {
+    dispatch(removeCartProduct(index));
+  }
+
   return (
     <MainComponent>
       <Row className="mb-4">
-        <Col lg={8} md={12} className="mt-4 mb-4">
+        <Col lg="8" md="12" className="mt-4 mb-4">
           <div className="mb-4">
             <strong>Meu carrinho</strong>
           </div>
@@ -24,79 +35,80 @@ const Cart: React.FC = () => {
             <strong>Produto</strong>
 
             <div className={styles.product}>
-              <Row>
-                <Col sm={4} xs={12}>
-                  <img 
-                    src="/assets/product-image.png" 
-                    alt="Jogo Counter Strike" 
-                    width={150} height={100} 
-                  />
-                </Col>
-
-                <Col sm={6} xs={6}>
-                  <h1 className={styles.title}>Counter Strike</h1>
-
-                  <div>
-                    <Badge 
-                      variant="primary ml-1" 
-                      className={styles.primary_badge}
+              {
+                cartProducts?.map(
+                  (product, index) =>
+                    <Row 
+                      key={index}
+                      className="mb-4"
                     >
-                        Ação
-                    </Badge>
+                      <Col sm="4" xs="12">
+                        <img 
+                          src={product.image_url}
+                          alt={product.name} 
+                          width={150}
+                          height={100}
+                        />
+                      </Col>
 
-                    <Badge 
-                      variant="primary ml-1" 
-                      className={styles.primary_badge}
-                    >
-                      Aventura
-                    </Badge>
+                      <Col sm="6" xs="6">
+                        <h1 className={styles.title}>{product.name}</h1>
 
-                    <Badge 
-                      variant="primary ml-1" 
-                      className={styles.primary_badge}
-                    >
-                      Indie
-                    </Badge>
-                  </div>
-                </Col>
+                        <div>
+                          {
+                            product?.categories?.map(
+                              category =>
+                              <Badge 
+                                key={category.id}
+                                className={styles.primary_badge} 
+                                variant="primary ml-1">
+                                  {category.name}
+                              </Badge>
+                            )
+                          }
+                        </div>
+                      </Col>
 
-                <Col sm={2} xs={6} className="text-center">
-                  <strong className="d-block">R$ 89.90</strong>
-                  <FontAwesomeIcon 
-                    icon={faTrash} 
-                    className={styles.icon}
-                  />
-                </Col>
-              </Row>
+                      <Col sm="2" xs="6" className="text-center">
+                        <strong className="d-block">{`R$ ${product.price}`}</strong>
+                        <FontAwesomeIcon 
+                          icon={faTrash} 
+                          className={styles.icon}
+                          onClick={() => handleRemove(index)}
+                        />
+                      </Col>
+                    </Row>
+                )
+              }
 
               <hr className={styles.line} />
 
               <div className={styles.coupon}>
                 <strong className="mr-3">
-                  Possui um cupom de desconto?
+                  Possui um cupom de desconto
                 </strong>
 
                 <input 
                   type="text" 
-                  className={styles.gray_input} 
-                  placeholder="EXEMPLO-DE-CUPOM" 
+                  className={styles.gray_input}
+                  placeholder="EXEMPLO-DE-CUPOM"
                 />
 
                 <StyledButton 
-                  action={"Aplicar"} 
+                  action="Aplicar" 
                   type_button="red" 
-                  className={styles.gray_button} 
+                  className={styles.gray_button}
                 />
               </div>
 
               <div className={styles.price_and_discount}>
                 <strong className="d-block">
-                  R$ 209.80
+                  {`R$ ${cartProducts?.reduce((acc, item) => acc + item.price, 0).toFixed(2)}`}
                 </strong>
 
                 <div>
                   <span className={styles.blue_text}>
-                    - 10% OFF + 
+                    - 10% OFF +
                   </span>
 
                   <strong className={styles.blue_text}>
@@ -118,12 +130,12 @@ const Cart: React.FC = () => {
           </BlueBackground>
         </Col>
 
-        <Col lg={4} md={12} className={styles.payment_column}>
+        <Col lg="4" md="12" className={styles.payment_column}>
           <div className={styles.back_button}>
             <div className="float-right">
-              <StyledButton
-                action={"Continuar Comprando"}
-                icon={faArrowLeft}
+              <StyledButton 
+                action="Continuar Comprando" 
+                icon={faArrowLeft} 
                 type_button="blue"
               />
             </div>
